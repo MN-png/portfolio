7601623678bd685a57be3967a00616378cfc72a4
MN-png <nafi8970@gmail.com>
2024-07-09 21:02:05 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,47 @@
+import project1 from '../assets/portfolioimage.jpg';
+import project2 from '../assets/quiz.jpg';
+
+export const HERO_CONTENT = `I'm a passionate Frontend Developer.I have honed my skill in frontend technologies are reactJS and Tailwind css.I'm a passionate Frontend Developer.I have honed my skill in frontend technologies are reactJS and Tailwind css.'m a passionate Frontend Developer.I have honed my skill in frontend technologies are reactJS and Tailwind css.`;
+
+
+export const ABOUT_ME = `I am dedicated and verstile frontend developer with passion for creating efficient and user-freindly web application.I am dedicated and verstile frontend developer with passion for creating efficient and user-freindly web application.I am dedicated and verstile frontend developer with passion for creating efficient and user-freindly web application.`;
+
+export const EXPERIANCE = [
+    {
+        year: "2023 - present",
+        role: "Senior Frontend developer",
+        company: "ABC Company",
+        place: "Mangalore",
+        Description: `Contributed to the development of web application using javascript, Tailwind CSS.`,
+        Technologies: ["ReactJS", "TailwindCSS"],
+    },
+    {
+        year: "2020 - 2023",
+        role: "Junior Frontend developer",
+        company: "XYZ Company",
+        place: "Bangalore",
+        Description: `Contributed to the development of web application using javascript, Tailwind CSS.`,
+        Technologies: ["ReactJS", "TailwindCSS"],
+    },
+];
+
+export const PROJECTS = [
+    {
+        title: "Portfolio",
+        image: project1,
+        description: "Fully responsive portfolio website",
+        technologies: ["ReactJS", "Tailwind CSS"]
+    },
+    {
+        title: "Quiz",
+        image: project2,
+        description: "Fully responsive Quiz website",
+        technologies: ["ReactJS", "Tailwind CSS"]
+    },
+];
+
+export const CONTACT = {
+        address: "6-83 A1 Merla gudde house, Someshwara uchil, Mangalore",
+        phoneNo: "+91 8970260000",
+        email: "nafi89@example.com"
+    };
